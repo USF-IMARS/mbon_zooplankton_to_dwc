2c9db0226774197c452b360dda3538afb1acd168
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-10-18 18:27:41 -0400

    function to create directories when starting projects

---
+++ scripts/create_dir.R
@@ -0,0 +1,26 @@
+# will create a set directory if does not exists
+# useful for new projects
+
+subDir_deflt <- c(
+    "data/raw",
+    "data/processed",
+    "data/plots",
+    "data/metadata",
+    "Rmd",
+    "scripts"
+)
+
+cust_dir <- c(
+    "data/processed/ind_file_merg"
+)
+subDir <- here::here(c(subDir_deflt, cust_dir))
+subDir <- subDir[which(!fs::dir_exists(subDir))]
+
+if (!length(subDir) == 0) {
+    cat("Creating directory(s):", subDir, sep = "\n\t")
+} else {
+    cat("No new directories created.")
+}
+
+fs::dir_create(path = subDir)
+rm(subDir_deflt, cust_dir, subDir)
