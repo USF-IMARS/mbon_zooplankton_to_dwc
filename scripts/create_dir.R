e4522e4a4e1f3113712b6a81c9acb671995a0803
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-10-24 23:45:50 -0400

    add ability to show tree structure of project

--- scripts/create_dir.R
+++ scripts/create_dir.R
@@ -1,6 +1,25 @@
 # will create a set directory if does not exists
 # useful for new projects
 
+showTree <- function(showtree = TRUE) {
+    treees <- data.frame(
+        stringsAsFactors = FALSE,
+        package = c(
+            here::here(),"data","raw","processed","plots",
+            "metadata", "Rmd", "scripts", "ind_file_merg", "aphia_id"
+        ),
+        dependences = I(
+            list(
+                c("data", "Rmd", "scripts"),
+                c("metadata", "plots", "processed", "raw"),
+                character(0), c("ind_file_merg"), character(0), c("aphia_id"),
+                character(0), character(0), character(0), character(0))))
+    if (showtree) {
+        cli::cli_text("Here is the {.strong project} structure:")
+        print(cli::tree(treees))
+    }
+}
+
 subDir_deflt <- c(
     "data/raw",
     "data/processed",
@@ -18,10 +37,15 @@ subDir <- here::here(c(subDir_deflt, cust_dir))
 subDir <- subDir[which(!fs::dir_exists(subDir))]
 
 if (!length(subDir) == 0) {
-    cat("Creating directory(s):", subDir, sep = "\n\t")
-} else {
-    cat("No new directories created.")
-}
+    cli::cli_alert_info("Creating {length(subDir)} director{?y/ies}.")
+    cli::cli_ul(subDir, .close = T)
+    showTree(showtree = TRUE)
+    } else {
+        cli::cli_alert_info("No new directories created.")
+        }
 
 fs::dir_create(path = subDir)
 rm(subDir_deflt, cust_dir, subDir)
+
+
+
