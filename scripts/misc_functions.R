dfc8e9254a63ce8e22cd409c250b85928ecc49d6
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-02-16 14:51:22 -0500

    changed location of description

--- scripts/misc_functions.R
+++ scripts/misc_functions.R
@@ -1,23 +1,21 @@
 # ============================================================================ #
 # ---- Misc Functions ----
 # ============================================================================ #    
-
-#' Returns Most Recent Created
-#'
-#' This function should be used after an `fs::dir_ls` search with a specific 
-#' file that may have multiple versions. When there are multiple matches to 
-#' a search, this will take the most recent version of it.
-#'
-#' @param fpath The fs_path object created from `fs::dir_ls`
-#' @param check Optionally check the most recent file. This can be set to 
-#' either `TRUE` or `FALSE`.
-#'
-#' @return A vector of the most recent created file as `fs_path` object
-#' @examples
-#' # NA
-#' 
 last_mod <-  function(fpath, check = TRUE) {
-    
+    #' Returns Most Recent Created
+    #'
+    #' This function should be used after an `fs::dir_ls` search with a specific 
+    #' file that may have multiple versions. When there are multiple matches to 
+    #' a search, this will take the most recent version of it.
+    #'
+    #' @param fpath The fs_path object created from `fs::dir_ls`
+    #' @param check Optionally check the most recent file. This can be set to 
+    #' either `TRUE` or `FALSE`.
+    #'
+    #' @return A vector of the most recent created file as `fs_path` object
+    #' @examples
+    #' # NA
+
     if (!check) return(fpath)
     
     ftime <- file.mtime(fpath)           
