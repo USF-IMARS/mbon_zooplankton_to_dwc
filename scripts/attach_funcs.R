30fbc60f00298a350558b9c9544ab95e30524ebb
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-02-16 11:06:17 -0500

    function to attach functions to new environment

---
+++ scripts/attach_funcs.R
@@ -0,0 +1,32 @@
+#' Attach All Custom Functions
+#'
+#' This will attach all the functions to the `searchpath` of `my_funcs` if it 
+#' is not already attached.
+#'
+#' @return Functions and scripts
+#' @examples
+#' # func_attach()
+func_attach <- function() {
+    if (!rlang::is_attached('my_funcs')) { 
+        cli::cli_alert_info(
+            c("Attaching project functions to the {.code searchpath} as ",
+              "{.code my_funcs}."))
+        
+        # source scripts with functions in new environment
+        my_funcs <- new.env()
+        
+        # create directory structure if does not exists and create tree map
+        source(here::here("scripts","create_dir.R"), local = my_funcs)
+        
+        # copy files from a cloud service
+        source(here::here("scripts", "copy_files_from_cloud.R"), local = my_funcs)
+        
+        # loading check for taxa 
+        source(here::here("scripts", "taxa_list_check.R"), local = my_funcs)
+        
+        attach(my_funcs)
+    } else {
+        cli::cli_alert_info(c("Skipping attach,{.var my_funcs} is already ", 
+                            "attached to the searchpath."))
+    }
+}
