1fa1c9b929dc94564bfdf39bdd78deaa43f7e36e
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-06-02 16:34:03 -0400

    add function to save csv files

--- scripts/misc_functions.R
+++ scripts/misc_functions.R
@@ -30,3 +30,133 @@ last_mod <-  function(fpath, check = TRUE) {
     
     # ---- end of function ----
 }
+
+##%######################################################%##
+#                                                          #
+####                   Save .csv File                   ####
+#                                                          #
+##%######################################################%##
+#' Save .csv File
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param .data The data.frame or tibble to be saved.
+#' @param save_location Folder to save file. Will create folder if doesn't exist.
+#' @param save_name Prefix name of file. Will be saved with `_<timestamp>.csv`
+#' @param overwrite `TRUE` or `FALSE` to re-save file if exists, or keep current. 
+#' @param verbose `TRUE` or `FALSE` to print location in script.
+#' @param time_stamp_fmt Timestamp format. 
+#'                       - default = YYYYMMDD_HHMMSS (i.e "%Y%m%d_%H%M%S")
+#'                       - if no suffix, `NULL`
+#'                       - if want custom, `<custom_message>`
+#'
+#' @return NULL, save file
+#' @examples
+#' # ADD_EXAMPLES_HERE
+save_csv <- function(
+        .data         = NULL,        
+        save_location = NULL,
+        save_name     = NULL,
+        overwrite     = FALSE,
+        verbose       = TRUE,
+        time_stamp_fmt = "%Y%m%d_%H%M%S") {
+    
+    # ---- checking input parameters
+    if (is.null(.data)) {
+        cli::cli_abort(
+            c("Check input, {.var {col_yellow(\".data\")}} is `NULL`.", 
+              "{col_red(\"Stopping\")}"))
+    }
+    if (is.null(save_location)) {
+        cli::cli_abort(
+            c("Check input, {.var {col_yellow(\"save_location\")}} is `NULL`.",
+              "{col_red(\"Stopping\")}"))
+    }
+    if (is.null(save_name)) {
+        cli::cli_abort(
+            c("Check input, {.var {col_yellow(\"save_name\")}} is `NULL`.",
+              "{col_red(\"Stopping\")}"))
+    }
+    if (is.null(overwrite)) {
+        cli::cli_abort(
+            c("Check input, {.var {col_yellow(\"overwrite\")}} is `NULL`.",
+              "{col_red(\"Stopping\")}"))
+    }
+
+    time_stamp_fmt <- 
+        tryCatch({
+            glue("_", format(Sys.time(), time_stamp_fmt))
+            }, error = function(e) {NULL})
+    
+    # ---- file name
+    data_f <-
+      here(
+        save_location,
+        glue(
+          "{save_name}",
+          time_stamp_fmt,
+          ".csv",
+          .null = ""
+        )
+      )
+    
+    print(head(.data))
+    
+    if (verbose) {
+        cli::cli_h1("{save_name}")
+        cli::cli_alert_info(
+            c("File Information:\n",
+              "Location:  {.file {save_location}}\n",
+              "File Name: {.file {basename(data_f)}}")
+            )
+    }
+    # ---- check if folder exists and create otherwise
+    if (!dir_exists(save_location)) {
+        
+        if (verbose) 
+            cli::cli_alert_info(
+                "{col_green(\"Creating\")} folder location!")
+        
+        fs::dir_create(save_location)
+    }
+
+    # ---- check if need to create file
+    file_loc <- 
+        fs::dir_ls(
+        save_location,
+        regexp = save_name
+        ) 
+    
+    create_f <- rlang::is_empty(file_loc)
+
+    if (!create_f && !overwrite) {
+        # return early if no need to create
+        if (verbose)
+            cli::cli_alert_info(
+            "File exist and {.emph is not} being {col_green(\"overwritten\")}!\n"
+        )
+        return(invisible())
+    }
+    
+    if (!create_f && overwrite && verbose) {
+        cli::cli_alert_info(
+            "File exist and {.emph is} being {col_red(\"overwritten\")}!")
+    } else if (create_f && verbose) {
+        cli::cli_alert_info("File does not exists, {col_green(\"creating\")}!")
+    }
+    
+    
+    # ---- saving file
+    if (verbose) cli::cli_alert_info("Saving file!")
+    
+    readr::write_csv(
+        x    = .data,
+        file = data_f,
+        na   = ""
+    )
+    
+    if (verbose) cli::cli_alert_success("Saved!\n\n")
+
+    # ---- end of function ----
+}
+
